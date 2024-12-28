b85988223ab56397561bdecaeb9beb38147944b2
jkostadinow <125033363+jkostadinow@users.noreply.github.com>
2024-12-28 20:17:31 +0200

    Create Program.cs

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/Foreign Languages/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Foreign_Languages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string counry = Console.ReadLine();
+
+            switch (counry)
+            {
+                case "USA":
+                case "England":
+                    Console.WriteLine("English");
+                    break;
+                case "Spain":
+                case "Argentina":
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
