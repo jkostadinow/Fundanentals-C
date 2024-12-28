a45a75cbe8be50aabe540a821d33891ff8fe1510
jkostadinow <125033363+jkostadinow@users.noreply.github.com>
2024-12-28 20:06:36 +0200

    Create Program.cs

---
+++ 02. CSharp-Fundamentals-Data-Types-and-Variables-Lab/8. Town Info/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+string townName = Console.ReadLine();
+int population = int.Parse(Console.ReadLine());
+int area = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Town {townName} has population of {population} and area {area} square km.");
