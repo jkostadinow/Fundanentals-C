7978cb813643dae9aaebb5fb9ad742276b9a5178
jkostadinow <125033363+jkostadinow@users.noreply.github.com>
2024-12-28 20:19:07 +0200

    Create Program.cs

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/Theatre Promotion/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Theatre_Promotion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine().ToLower();
+            int age = int.Parse(Console.ReadLine());
+
+            if (age >= 0 && age <= 18)
+            {
+                if (day == "weekday")
+                {
+                    Console.WriteLine("12$");
+                }
+                else if (day == "weekend")
+                {
+                    Console.WriteLine("15$");
+                }
+                else if (day == "holiday")
+                {
+                    Console.WriteLine("5$");
+                }
+            }
+            else if (age > 18 && age <= 64)
+            {
+                if (day == "weekday")
+                {
+                    Console.WriteLine("18$");
+                }
+                else if (day == "weekend")
+                {
+                    Console.WriteLine("20$");
+                }
+                else if (day == "holiday")
+                {
+                    Console.WriteLine("12$");
+                }
+            }
+            else if (age > 64 && age <= 122)
+            {
+                if (day == "weekday")
+                {
+                    Console.WriteLine("12$");
+                }
+                else if (day == "weekend")
+                {
+                    Console.WriteLine("15$");
+                }
+                else if (day == "holiday")
+                {
+                    Console.WriteLine("10$");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
