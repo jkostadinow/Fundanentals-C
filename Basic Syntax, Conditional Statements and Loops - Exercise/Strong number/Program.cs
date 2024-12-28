f6e8c8b0d43579786aa25238dd66703e29cc8a0c
jkostadinow <125033363+jkostadinow@users.noreply.github.com>
2024-12-28 20:25:47 +0200

    Create Program.cs

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/Strong number/Program.cs
@@ -0,0 +1,24 @@
+﻿int number = int.Parse(Console.ReadLine());
+int numberCopy = number;
+int sum = 0;
+
+while (numberCopy > 0)
+{
+    int digit = numberCopy % 10;
+    numberCopy = numberCopy / 10;
+    int factorial = 1;
+    for (int i = 1; i <= digit; i++)
+    {
+        factorial *= i;
+    }
+    sum += factorial;
+}
+if (sum == number)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
+
