cb65056bf3826ecdd882ca23473efb1ed93ae29d
jkostadinow <125033363+jkostadinow@users.noreply.github.com>
2024-12-28 20:05:01 +0200

    Create Program.cs

---
+++ 02. CSharp-Fundamentals-Data-Types-and-Variables-Lab/11. Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+
+Console.WriteLine("Length: ");
+double length = double.Parse(Console.ReadLine());
+Console.WriteLine("Width: ");
+double width = double.Parse(Console.ReadLine());
+Console.WriteLine("Heigth: ");
+double heigth = double.Parse(Console.ReadLine());
+double V = (length + width + heigth) / 3;
+Console.WriteLine($"Pyramid Volume: {V:f2}");
