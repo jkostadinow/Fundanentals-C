d4bd6fcf1f2817260afeba3514e07fb6519af85c
jkostadinow <125033363+jkostadinow@users.noreply.github.com>
2024-12-28 20:18:04 +0200

    Create Program.cs

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/Multiplication Table 2.0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Multiplication_Table_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int startIndex = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine($"{n} X {startIndex} = {n * startIndex}");
+                startIndex++;
+            }while (startIndex <= 10);
+        }
+    }
+}
