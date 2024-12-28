5576cbe7a3983bda0d65b8eca43b3765518fd667
jkostadinow <125033363+jkostadinow@users.noreply.github.com>
2024-12-28 20:25:16 +0200

    Create Program.cs

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/Orders/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Transactions;
+
+int orders = int.Parse(Console.ReadLine());
+double totalPrice = 0;
+for (int i = 0; i < orders; i++)
+{
+    double orderPrice = 0;
+    double capsulePrice = double.Parse(Console.ReadLine());
+    int days = int.Parse(Console.ReadLine());
+    int capsuleCount = int.Parse(Console.ReadLine());
+    orderPrice = days * capsuleCount * capsulePrice;
+    totalPrice += orderPrice;
+    Console.WriteLine($"The price for the coffee is: ${orderPrice:f2}");
+}
+Console.WriteLine($"Total: ${totalPrice:f2}");
