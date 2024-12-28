7b667c82d977f085770f04db57b96492c367a201
jkostadinow <125033363+jkostadinow@users.noreply.github.com>
2024-12-28 20:25:41 +0200

    Create Program.cs

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/Rage Expenses/Program.cs
@@ -0,0 +1,53 @@
+﻿int lostGamesCount = int.Parse(Console.ReadLine());
+double headsetPrice = double.Parse(Console.ReadLine());
+double mousePrice = double.Parse(Console.ReadLine());
+double keyboardPrice = double.Parse(Console.ReadLine());
+double displayPrice = double.Parse(Console.ReadLine());
+
+int counterGameForBrokenHeadset = 0;
+int counterGameForBrokenMouse = 0;
+int counterGameForBrokenKeyboard = 0;
+
+bool headsetBroken = false;
+bool mouseBroken = false;
+
+double price = 0;
+for (int i = 1; i <= lostGamesCount; i++)
+{
+    counterGameForBrokenHeadset++;
+    counterGameForBrokenMouse++;
+    if (counterGameForBrokenHeadset == 2)
+    {
+        headsetBroken = true;
+        counterGameForBrokenHeadset = 0;
+        price += headsetPrice;
+    }
+    else
+    {
+        headsetBroken = false;
+    }
+    if (counterGameForBrokenMouse == 3)
+    {
+        mouseBroken = true;
+        counterGameForBrokenMouse = 0;
+        price += mousePrice;
+    }
+    else
+    {
+        mouseBroken = false;
+    }
+    if (headsetBroken && mouseBroken)
+    {
+        headsetBroken = false;
+        mouseBroken = false;
+        counterGameForBrokenKeyboard++;
+        price += keyboardPrice;
+    }
+    if (counterGameForBrokenKeyboard == 2)
+    {
+        counterGameForBrokenKeyboard = 0;
+        price += displayPrice;
+    }
+
+}
+Console.WriteLine($"Rage expenses: {price:f2} lv.");
