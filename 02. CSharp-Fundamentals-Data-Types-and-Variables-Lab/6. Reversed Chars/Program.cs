58e4e20bbe6122bff761332f5698bcb4792ed689
jkostadinow <125033363+jkostadinow@users.noreply.github.com>
2024-12-28 20:06:10 +0200

    Create Program.cs

---
+++ 02. CSharp-Fundamentals-Data-Types-and-Variables-Lab/6. Reversed Chars/Program.cs
@@ -0,0 +1,6 @@
+﻿
+char line1 = char.Parse(Console.ReadLine());
+char line2 = char.Parse(Console.ReadLine());
+char line3 = char.Parse(Console.ReadLine());
+
+Console.WriteLine($"{line3} {line2} {line1}");
