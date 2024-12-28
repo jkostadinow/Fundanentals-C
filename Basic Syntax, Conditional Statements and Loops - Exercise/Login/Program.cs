9b4629583b1192d79c95c520a6d0b5367281280f
jkostadinow <125033363+jkostadinow@users.noreply.github.com>
2024-12-28 20:25:10 +0200

    Create Program.cs

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/Login/Program.cs
@@ -0,0 +1,24 @@
+﻿string userName = Console.ReadLine();
+string expectedPassword = new string(userName.Reverse().ToArray());
+int loginAttepts = 0;
+
+while (true)
+{
+    string providedPassword = Console.ReadLine();
+    if (providedPassword == expectedPassword)
+    {
+        Console.WriteLine($"User {userName} logged in.");
+        return;
+    }
+    else
+    {
+        loginAttepts++;
+        Console.WriteLine("Incorrect password. Try again.");
+    }
+    if (loginAttepts == 3)
+    {
+        Console.WriteLine($"User {userName} blocked!");
+        break;
+    }
+}
+
