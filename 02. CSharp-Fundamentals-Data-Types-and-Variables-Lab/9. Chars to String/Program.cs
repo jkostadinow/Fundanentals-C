62bda97ec3f05d2b22d212e1628db81d043d5af8
jkostadinow <125033363+jkostadinow@users.noreply.github.com>
2024-12-28 20:06:45 +0200

    Create Program.cs

---
+++ 02. CSharp-Fundamentals-Data-Types-and-Variables-Lab/9. Chars to String/Program.cs
@@ -0,0 +1,8 @@
+﻿
+char line1 = char.Parse(Console.ReadLine());
+char line2 = char.Parse(Console.ReadLine());
+char line3 = char.Parse(Console.ReadLine());
+
+
+string result = $"{line1}{line2}{line3}";
+Console.WriteLine(result);
