8ce04723c866e8a7d30687ffefa8f3b0563526af
jkostadinow <125033363+jkostadinow@users.noreply.github.com>
2024-12-28 20:05:37 +0200

    Create Program.cs

---
+++ 02. CSharp-Fundamentals-Data-Types-and-Variables-Lab/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+int currentNumber = 0;
+bool isSpecial = false;
+for (int ch = 1; ch <= number; ch++)
+{
+    currentNumber = ch;
+    while (ch > 0)
+    {
+        sum += ch % 10;
+        ch = ch / 10;
+    }
+    isSpecial = (sum == 5) || (sum == 7) || (sum == 11);
+    Console.WriteLine("{0} -> {1}", currentNumber, isSpecial);
+    sum = 0;
+    ch = currentNumber;
+}
