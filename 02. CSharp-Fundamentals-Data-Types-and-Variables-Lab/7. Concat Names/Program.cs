bd8a52e071ecdbf9a0a80f1cc4167e0000d1b291
jkostadinow <125033363+jkostadinow@users.noreply.github.com>
2024-12-28 20:06:24 +0200

    Create Program.cs

---
+++ 02. CSharp-Fundamentals-Data-Types-and-Variables-Lab/7. Concat Names/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+
+
+string firstName = Console.ReadLine();
+string secondName = Console.ReadLine();
+string delimiter = Console.ReadLine();
+
+Console.WriteLine($"{firstName}{delimiter}{secondName}");
